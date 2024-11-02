96efce737b8d984cce2b5911df88ea326d69d2f8
Nam Nguyen <namy.ngu@gmail.com>
2024-11-02 18:41:33 +1100

    Update

--- app/src/main/java/com/example/themovieapp/model/Movie.java
+++ app/src/main/java/com/example/themovieapp/model/Movie.java
@@ -1,10 +1,19 @@
 package com.example.themovieapp.model;
 
+import android.widget.ImageView;
+
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+import androidx.databinding.BindingAdapter;
+
 import java.util.List;
+
+import com.bumptech.glide.Glide;
+import com.example.themovieapp.BR;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-public class Movie {
+public class Movie extends BaseObservable {
 
     @SerializedName("id")
     @Expose
@@ -17,6 +26,16 @@ public class Movie {
     @SerializedName("poster_path")
     @Expose
     private String posterPath;
+
+    @BindingAdapter({"posterPath"})
+    public static void loadImage(ImageView imageView, String imageUrl) {
+        // Basic Url: "https://image.tmdb.org/t/p/w500/"
+        String imagePath = "https://image.tmdb.org/t/p/w500/" + imageUrl;
+
+        Glide.with(imageView.getContext())
+                .load(imagePath)
+                .into(imageView);
+    }
 
     @SerializedName("release_date")
     @Expose
@@ -62,20 +81,24 @@ public class Movie {
         this.releaseDate = releaseDate;
     }
 
+    @Bindable
     public String getTitle() {
         return title;
     }
 
     public void setTitle(String title) {
         this.title = title;
+        notifyPropertyChanged(BR.title);
     }
 
+    @Bindable
     public Double getVoteAverage() {
         return voteAverage;
     }
 
     public void setVoteAverage(Double voteAverage) {
         this.voteAverage = voteAverage;
+        notifyPropertyChanged(BR.voteAverage);
     }
 
 }
--- app/src/main/java/com/example/themovieapp/model/MovieRespository.java
+++
@@ -1,4 +0,0 @@
-package com.example.themovieapp.model;
-
-public class MovieRespository {
-}
